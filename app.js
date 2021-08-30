11e82e30c22c7429c6755829319944966d6ff15f
aniketsingh13 <85512784+aniketsingh13@users.noreply.github.com>
2021-08-30 04:30:51 -0700

    completing js

--- app.js
+++ app.js
@@ -2,5 +2,20 @@
 var randomNumber1= Math.round(Math.random() * 6);
 
   var randomSourceImage="images/dice" + randomNumber1    +".png" ;
-  var image3 = document.querySelectorAll("img")[0];
-  image3.setAttribute("src", randomSourceImage); 
+  var image1 = document.querySelectorAll("img")[0];
+  image1.setAttribute("src", randomSourceImage);
+    
+  var randomNumber2= Math.round(Math.random() * 6);
+  var randomSoureceImage2= "images/dice" +randomNumber2 + ".png";
+  var image2 = document.querySelectorAll("img")[1];
+  image2.setAttribute("src" ,randomSoureceImage2);
+
+  if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML="🎌 player 1 wins"
+  }
+  else if(randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML=" player 2 wins 🎌"
+  }
+  else{
+    document.querySelector("h1").innerHTML= "Draw 🏁"
+  }
